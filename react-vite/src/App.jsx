bd24bfae50af6b2e251dc5c961aaedb469ff7403
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-11-27 19:06:25 +0530

    commit finalsub

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import ReactDOM, { unstable_renderSubtreeIntoContainer } from 'react-dom';
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+function App() {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  )
+  const element = (
+    <div>
+      <h1>Hello World, The Time Is</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
     </div>
   )
+  root.render(element)
 }
-
+setInterval(App, 1000)
 export default App
