01218e9e875c024589d366dbee4520e3208c4963
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-12-11 13:21:52 +0530

    commit reacct

--- react-vite/src/components/Header.jsx
+++ react-vite/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import img from '../assets/pexels-hristo-fidanov-1252869.jpg'
+import img from '../assets/pexels-mont-photographs-2948636.jpg'
 import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <img src={img} alt="" srcset="" />
+        <div className='linearGradient'>
+            <div className="classLinear"></div>
+            <h1>The Design You Fell In <br /> Love With</h1>
+            <img src={img} />
         </div>
     )
 }
