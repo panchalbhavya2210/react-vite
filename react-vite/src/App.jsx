62960a923b352c99c0a7376d3618f5a937660097
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-12-11 12:21:11 +0530

    commit uk

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from 'react'
-import ReactDOM, { unstable_renderSubtreeIntoContainer } from 'react-dom';
-import reactLogo from './assets/react.svg'
-import header from './components/Header'
 import './App.css'
 import Header from './components/Header';
 
--- react-vite/src/components/Header.jsx
+++ react-vite/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import img from '../assets/pexels-hristo-fidanov-1252869.jpg'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>This is header</div>
+        <div>
+            <img src={img} alt="" srcset="" />
+        </div>
     )
 }
 
