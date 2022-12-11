eb4d487c1bc92cec8bbae0b0395bea0b48b959ef
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-12-11 12:04:58 +0530

    header push

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,20 +1,15 @@
 import React, { useState } from 'react'
 import ReactDOM, { unstable_renderSubtreeIntoContainer } from 'react-dom';
 import reactLogo from './assets/react.svg'
+import header from './components/Header'
 import './App.css'
+import Header from './components/Header';
 
 
 function App() {
-  const root = ReactDOM.createRoot(
-    document.getElementById('root')
+  return (
+    <Header />
   )
-  const element = (
-    <div>
-      <h1>Hello World, The Time Is</h1>
-      <h2>{new Date().toLocaleTimeString()}</h2>
-    </div>
-  )
-  root.render(element)
 }
-setInterval(App, 1000)
+
 export default App
