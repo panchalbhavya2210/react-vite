6922767b5783ec0bee73ff768e9640a3b5257888
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2022-12-11 15:25:16 +0530

    commit 10%

--- react-vite/src/components/Header.jsx
+++ react-vite/src/components/Header.jsx
@@ -3,10 +3,25 @@ import img from '../assets/pexels-mont-photographs-2948636.jpg'
 import './Header.css'
 
 const Header = () => {
+    window.onscroll = function () {
+        let windowTextPos = window.scrollY;
+        console.log(windowTextPos)
+
+        if (windowTextPos < 200) {
+            let thisHead = document.getElementById('thisHead');
+
+            thisHead.style.transform = `translateY(-${windowTextPos}px)`;
+        }
+        else {
+            null
+        }
+    }
     return (
         <div className='linearGradient'>
             <div className="classLinear"></div>
-            <h1>The Design You Fell In <br /> Love With</h1>
+            <div className="headHolder" id='thisHead'>
+                <h1 className='headHolder'>The Design You Can Fell In <br /> Love With.</h1>
+            </div>
             <img src={img} />
         </div>
     )
